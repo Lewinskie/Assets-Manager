e64492b6a09c3ad30e337a84402429adb05d8833
Lewinskie <wangaralewis@gmail.com>
2023-11-14 08:02:57 +0300

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require("express");
-
 const { ApolloServer, gql } = require("apollo-server-express");
-
-const app = express();
-const port = 4000;
-
+// const typeDefs = require('./path-to-your-schema'); // Replace with the path to your GraphQL schema
+// const resolvers = require('./path-to-your-resolvers'); // Replace with the path to your GraphQL resolvers
 const typeDefs = gql`
   type Query {
     hello: String!
@@ -17,10 +14,27 @@ const resolvers = {
   },
 };
 
+// Create an Apollo Server instance
 const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app });
-app.listen({ port }, () =>
-  console.log(
-    `🚀 Server ready at http://localhost:ocalhost:${port}${server.graphqlPath}`
-  )
-);
+
+// Create an Express App
+const app = express();
+
+// Start the Apollo Server
+async function startServer() {
+  await server.start();
+
+  // Apply the Apollo Server middleware to the Express app
+  server.applyMiddleware({ app });
+
+  // Start the Express server
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running at http://localhost:${PORT}/graphql`);
+  });
+}
+
+// Call the function to start the server
+startServer().catch((error) => {
+  console.error("Error starting the server:", error);
+});
